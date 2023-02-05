6438b732f0e0026da5326c17808da3a0d5851cfd
Shaddow798 <37914686+Shaddow798@users.noreply.github.com>
2023-02-05 17:49:34 +1300

    start adding customiaztion

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
-@app.route('/')
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/settings', methods=['POST', 'GET'])
+def settings():
+    return render_template('settings.html')
+
 
 if __name__ == "__main__":
     app.run()
