db24b4b036125bcf9629f95ac1865f1368a0e738
Shaddow798 <37914686+Shaddow798@users.noreply.github.com>
2023-02-07 17:00:16 +1300

    add the database enterys

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ db = SQLAlchemy(app)
 
 class Icons(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    # Content is equal to to name of the link
+    content = db.Column(db.String(200), nullable=False)
+    content_link = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
 
 
 
