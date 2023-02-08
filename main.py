f905d23b71ae4e3cd851538e71d81667af165953
Shaddow798 <37914686+Shaddow798@users.noreply.github.com>
2023-02-08 14:28:56 +1300

    changes to try to fix issues

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Icons(db.Model):
     # Content is equal to to name of the link
     content = db.Column(db.String(100), nullable=False)
     content_link = db.Column(db.String(200), nullable=False)
-    content_order = db.Column(db.Integer, nullable=False)
+    content_order = db.Column(db.Integer, nullable=True)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     
     def __repr__(self):
@@ -38,9 +38,14 @@ def settings():
     if request.method == 'POST':
         pin_content = request.form['content']
         pin_link_content = request.form['link_content']
+        pin_order = request.form['order']
         new_pin = Icons(content=pin_content)
+        new_link_pin = Icons(content=pin_link_content)
+        new_order = Icons(content=pin_order)
         try:
             db.session.add(new_pin)
+            db.session.add(new_link_pin)
+            db.session.add(new_order)
             db.session.commit()
             return redirect('/settings')
         except:
