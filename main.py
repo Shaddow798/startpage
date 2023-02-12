baeae216943382e4f08d9767fbd7687ab81a1a54
Shaddow798 <37914686+Shaddow798@users.noreply.github.com>
2023-02-12 22:13:21 +1300

    rename it and hope

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+import os
 # define basic things such as the config file
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
