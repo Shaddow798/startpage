65bae00c2d038e28e0a845cf875d0fd960331f26
Shaddow798 <37914686+Shaddow798@users.noreply.github.com>
2023-02-18 22:04:45 +1300

    get google search working

--- static/settings.js
+++ static/settings.js
@@ -2,4 +2,11 @@ alert("Hello")
 
 // start writing the scirpt to change the search engine
 
-var search = "duckduckgo"
+var search = "google"
+if (search == "duckduckgo") {
+    pass
+}
+else if (search == "google") {
+    document.getElementById("searchbar").action = "https://www.google.com/search?q="; //Will set it
+    document.getElementsByClass('form-control').placeholder='new text for fname';
+}
