18e41c0e3e79d2a8de3ee3c1d6e5a3bc2984791d
Shaddow798 <37914686+Shaddow798@users.noreply.github.com>
2023-02-19 15:30:06 +1300

    get search working

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,22 @@
 alert("Hello")
 
 // start writing the scirpt to change the search engine
-localStorage.setItem("search", "google");
+//localStorage.setItem("search", "google");
 var search = localStorage.getItem("search");
 
 if (search == "duckduckgo") {
-    pass
+    document.getElementById("searchbar").action = "https://duckduckgo.com/?q="; //Will set it
+    document.getElementsByClass('form-control').placeholder='new text for fname';
 }
 else if (search == "google") {
     document.getElementById("searchbar").action = "https://www.google.com/search?q="; //Will set it
+    document.getElementsByClass('form-control').placeholder='new text for fname';
+}
+else if (search == "yahoo") {
+    document.getElementById("searchbar").action = "https://www.search.yahoo.com/search?p="; //Will set it
+    document.getElementsByClass('form-control').placeholder='new text for fname';
+}
+else if (search == "bing") {
+    document.getElementById("searchbar").action = "https://www.bing.com/search?q="; //Will set it
     document.getElementsByClass('form-control').placeholder='new text for fname';
 }
--- static/js/settings.js
+++ static/js/settings.js
@@ -1,1 +1,10 @@
-var search = localStorage.getItem("search");
+function setsearch() {
+    search = document.getElementById("searchengine"),
+    searchvalue = search.value;
+    localStorage.setItem("search", searchvalue);
+    alert("Your setting has been applied sucsesfully")
+}
+
+
+
+// var search = localStorage.getItem("search");
