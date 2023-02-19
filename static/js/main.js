55da788480f08b6ca023d7078229db8eb5d3a28e
Shaddow798 <37914686+Shaddow798@users.noreply.github.com>
2023-02-20 09:10:30 +1300

    Update the searchbox to fix the text displayed

--- static/js/main.js
+++ static/js/main.js
@@ -8,21 +8,21 @@ document.getElementsByTagName('body')[0].style = 'background: #333;';
 //Manual overide for setting the search engine
 //localStorage.setItem("search", "google");
 var search = localStorage.getItem("search");
+document.getElementById("searchtext").placeholder = "d"
 
 if (search == "duckduckgo") {
     document.getElementById("searchbar").action = "https://duckduckgo.com/?q="; //Will set it
-    document.getElementsByClass('form-control').placeholder='new text for fname';
+    document.getElementById("searchtext").placeholder = "Search With DuckDuckgo."
 }
 else if (search == "google") {
     document.getElementById("searchbar").action = "https://www.google.com/search?q="; //Will set it
-    document.getElementsByClass('form-control').placeholder='new text for fname';
+    document.getElementById("searchtext").placeholder = "Search With Google."
 }
 else if (search == "yahoo") {
     document.getElementById("searchbar").action = "https://www.search.yahoo.com/search?p="; //Will set it
-//    document.getElementsByClass('form-control').placeholder='new text for fname';
-
+    document.getElementById("searchtext").placeholder = "Search With Yahoo."
 }
 else if (search == "bing") {
     document.getElementById("searchbar").action = "https://www.bing.com/search?q="; //Will set it
-//    document.getElementsByClass('form-control').placeholder='new text for fname';
+    document.getElementById("searchtext").placeholder = "Search With Bing."
 }
