86ebe3d8a02d84c6892aefaa7a4710ea6ea445bb
Shaddow798 <37914686+Shaddow798@users.noreply.github.com>
2023-03-13 17:37:15 +1300

    completly strip out sql database

--- main.py
+++ main.py
@@ -1,44 +1,17 @@
 from flask import Flask, render_template, url_for, request, redirect, send_from_directory
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 from datetime import datetime
 import os
-import base64
-
-#base 64 is for filenames 
-
-
 
 
 UPLOAD_FOLDER = 'static/images/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
+
 # define basic things such as the config file
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-# Define where the database is.
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///icons.db'
-app.app_context()
-db = SQLAlchemy(app)
-
-
-# Setup what each thing is in the database
-class Icons(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    # Content is equal to to name of the link
-    content = db.Column(db.String(100), nullable=False)
-    content_link = db.Column(db.String(200), nullable=False)
-    content_order = db.Column(db.Integer, nullable=True)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-
-    def __repr__(self):
-        return '<Pins %r>' % self.id
-
-# Trying to get this to work but its being a bitch.
 
 
 def allowed_file(filename):
@@ -49,7 +22,6 @@ def allowed_file(filename):
 # Routes for every page and what the function is
 @app.route('/', methods=['GET'])
 def index():
-    #pins = Icons.query.order_by(Icons.date_created).all()
     return render_template('index.html')
 
 
@@ -57,26 +29,10 @@ def index():
 # setuo the settings 
 @app.route('/settings', methods=['POST', 'GET'])
 def settings():
-    if request.method == 'POST':
-        pin_content = request.form['content']
-        pin_link_content = request.form['link_content']
-        pin_order = request.form['order']
-        new_pin = Icons(content=pin_content)
-        new_link_pin = Icons(content=pin_link_content)
-        new_order = Icons(content=pin_order)
-        try:
-            db.session.add(new_pin)
-            db.session.add(new_link_pin)
-            db.session.add(new_order)
-            db.session.commit()
-            return redirect('/settings')
-        except:
-            return 'Something went wrong adding your item to the database'
-    else:
-        return render_template('settings.html')
+    return render_template('settings.html')
+
 
 # File uploader test
-
 @app.route('/fileuploader', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
@@ -93,7 +49,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            #return redirect(url_for('download_file', name=filename))
+            # return redirect(url_for('download_file', name=filename))
             return redirect('/', background=filename)
     return '''
     <!doctype html>
