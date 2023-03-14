085126b3ceb5463dda87cc385bc9a5973026c736
Shaddow798 <37914686+Shaddow798@users.noreply.github.com>
2023-03-14 20:57:33 +1300

    Update main.py

--- main.py
+++ main.py
@@ -40,42 +40,6 @@ def settings():
     return render_template('settings.html')
 
 
-# File uploader test
-@app.route('/fileuploader', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # return redirect(url_for('download_file', name=filename))
-            return redirect('/', background=filename)
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
-
-# This allows fot the file uploader to open the file after being uploadsed
-
-#@app.route('/uploads/<name>')
-#def download_file(name):
-#    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
-
-
 @app.route('/about')
 def about():
     return render_template(about.html)
