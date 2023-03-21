9ebad0bdbf7fff15ba560005a780d5dcda247498
Shaddow798 <37914686+Shaddow798@users.noreply.github.com>
2023-03-21 22:27:09 +1300

    set debuging on

--- main.py
+++ main.py
@@ -92,4 +92,4 @@ def internal_error(error):
 # All this does it run it aslong as its not being imported
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
